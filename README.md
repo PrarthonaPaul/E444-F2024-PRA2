3f17c324b2d4b09c4b5519b85aa8f882b7f1b76d
Prarthona Paul <p.paul@mail.utoronto.ca>
2024-09-24 21:34:48 -0400

    activity 1.3

--- README.md
+++ README.md
@@ -18,6 +18,15 @@ This repo is a clone of https://github.com/miguelgrinberg/flasky
   </kbd>
 </p>
 
+### Activity 1.3
+<p align="center"> 
+  <kbd>
+    <a href="http://127.0.0.1:5000/" target="_blank"><img src="images/activity1-3.png">
+    <a href="http://127.0.0.1:5000/user/Prarthona" target="_blank"><img src="images/activity1-3-2.png">
+  </a>
+  </kbd>
+</p>
+
 This repository contains the source code examples for the second edition of my O'Reilly book [Flask Web Development](http://www.flaskbook.com).
 
 The commits and tags in this repository were carefully created to match the sequence in which concepts are presented in the book. Please read the section titled "How to Work with the Example Code" in the book's preface for instructions.
--- example-app/hello.py
+++ example-app/hello.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
-def index():
-	return '<h1>Hello World!</h1>'
+def home():
+	current_time = datetime.now().strftime("%A, %B %d, %Y %I:%M %p")
+	return render_template("index.html")
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, {}!</h1>'.format(name)
+    # Get the current date and time
+    now = datetime.utcnow()
+    # Send current time to the template
+    return render_template('user.html', name=name, current_time=now)
+
+if __name__ == '__main__':
+    app.run(debug=True)
